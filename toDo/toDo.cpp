f6dc72b06d69bc3d5dac37bd3c8b8eb80c701b84
Erick Bajaña <erick.bajana@epn.edu.ec>
2025-08-10 12:29:07 -0500

    Added 2 new functionalities

--- toDo/toDo.cpp
+++ toDo/toDo.cpp
@@ -1,19 +1,22 @@
-#include "toDo.h"
+
 #include <iostream>
 #include <fstream>
 #include <ctime>
 #include <string>
-#include <iomanip>
+#include <cctype>
+#include <algorithm>
 #include <sstream>
+#include <iomanip>
+#include "toDo.h"
 
-using namespace std;
-int N_TAREA = 1;
+
+int N_TASK = 1;
 
 struct Node {
-    int id = N_TAREA;
-    string description;
+    int id = N_TASK;
+    std::string description;
     bool status = false;
-    string dateCreated;
+    std::string dateCreated;
     Node *next = nullptr;
 
 };
@@ -30,42 +33,22 @@ int main(){
 
 
 void mainMenu(){
-    cout << "*****LISTA DE TAREAS PENDIENTES *********" <<endl;
-    cout << "1. Agregar una tarea a la lista" <<endl;
-    cout << "2. Eliminar una tarea por ID" <<endl;
-    cout << "3. Buscar tareas con palabra clave" <<endl;
-    cout << "4. Marcar tarea como completada" <<endl;
-    cout << "5. Mostrar todas las tareas" <<endl; 
-    cout << "6. Vaciar la lista" <<endl;
-    cout << "7. Cargar tareas desde archivo" <<endl;  
-    cout << "8. Volver al menu principal" <<endl;
-    cout << "Ingrese una opcion: ";
+    std::cout << "*****LISTA DE TAREAS PENDIENTES *********" <<std::endl;
+    std::cout << "1. Agregar una tarea a la lista" <<std::endl;
+    std:: cout <<"2. Eliminar una tarea por ID" <<std::endl;
+    std::cout << "3. Buscar tareas con palabra clave" <<std::endl;
+    std::cout << "4. Marcar tarea como completada" <<std::endl;
+    std::cout << "5. Mostrar todas las tareas" <<std::endl; 
+    std::cout << "6. Vaciar la lista" <<std::endl;
+    std::cout << "7. Guardar y volver al menu principal" <<std::endl;
+    std::cout << "Ingrese una opcion: ";
 
 }
-
-void emptyList(List *list){
-    Node * current = list->head;
-    Node * temp;
-    while(current != nullptr){
-        temp = current;
-        current = current->next;
-        delete temp;
-         
-
-    }
-    list->head = nullptr;
-    cout << "La lista ha sido vaciada correctamente." << endl;
-    cout << "PRESIONE UNA TECLA PARA CONTINUAR...."<< endl;
-    cin.get();
-
-}
-
-
-void addTask(List *& list, string description){
+void addTask(List *& list, std::string description){
 
     Node* toAdd = new Node();
     toAdd->description = description;
-    string currentTime = getTimeCustom();
+    std::string currentTime = getTimeCustom();
     toAdd->dateCreated = currentTime;
     toAdd->next = nullptr;
     
@@ -83,13 +66,13 @@ void addTask(List *& list, string description){
     current->next = toAdd;
     }
 
-    N_TAREA++;
+    N_TASK++;
     
-    cout << "Tarea añadida con exito!!" <<endl;
-    cout << "Lista de tareas en la lista: " << endl;
+    std::cout << "Tarea añadida con exito!!" <<std::endl;
+    std::cout << "Lista de tareas en la lista: " << std::endl;
     showTasks(list);
-    cout << "PRESIONE UNA TECLA PARA CONTINUAR...."<< endl;
-    cin.get();
+    std::cout << "PRESIONE UNA TECLA PARA CONTINUAR...."<< std::endl;
+    std::cin.get();
 
 }
 
@@ -99,29 +82,30 @@ void deleteTask(List *& list, int ID){
     char confirmation;
     
     while (current != nullptr){
-        if(current->id = ID){
-            cout << "DESCRIPCIÓN DE LA TAREA A ELIMINAR:\n";
-            cout << current->description << endl;
-            cout << "CREADA EL: " << current->dateCreated << endl;
-            cout << "¿Desea eliminar esta tarea? [Y/n]: ";
-            cin >> confirmation;
-            if(confirmation != 'n'){
+        if(current->id == ID){
+            std::cout << "DESCRIPCIÓN DE LA TAREA A ELIMINAR:\n";
+            std::cout << current->description << std::endl;
+            std::cout << "CREADA EL: " << current->dateCreated << std::endl;
+            std::cout << "¿Desea eliminar esta tarea? [Y/n]: ";
+            std::cin >> confirmation;
+            if(confirmation == 'y' || confirmation =='Y'){
                 // first node to eliminate is the head
                 if(previous == nullptr){
                     list->head = current->next;
                 }else{
-                    previous->next = current->next;
+                    previous->next= current->next;
+
                 }
                 delete current;
                 
             }else{
-                cout << "Eliminación cancelada. Presione una tecla para continuar..." << endl;
-                cin.ignore(); cin.get();
+                std::cout << "Eliminación cancelada. Presione una tecla para continuar..." << std::endl;
+                std::cin.get();
                 return;
             }
-            cout << "TAREA ELIMINADA CON EXITO" << endl;
-            cout << "PRESIONE UNA TECLA PARA CONTINUAR...." << endl;
-            cin.get();
+            std::cout << "TAREA ELIMINADA CON EXITO" << std::endl;
+            std::cout << "PRESIONE UNA TECLA PARA CONTINUAR...." << std::endl;
+            std::cin.get();
             
             return;
         }
@@ -129,77 +113,157 @@ void deleteTask(List *& list, int ID){
         current = current->next;
         
     }
-    cout << "LA TAREA CON ESE ID NO EXISTE" << endl;
+    std::cout << "LA TAREA CON ESE ID NO EXISTE" << std::endl;
 
 }
 
-string getTimeCustom() {
-    time_t now = time(nullptr);
-    tm* localTime = localtime(&now);
-    ostringstream oss;
-    oss <<setfill('0')
-        <<setw(2) << localTime->tm_mday << "/"
-        <<setw(2) << (localTime->tm_mon + 1) << "/"
-        << (localTime->tm_year + 1900)
-        << " -> "
-        <<setw(2) << localTime->tm_hour << ":"
-        <<setw(2) << localTime->tm_min;
+void lookForTask(List *list, std::string &keyword){
+    Node* current = list->head;
+    bool founded = false;
+    std::cout << "TAREAS QUE CONTIENEN: \" " << keyword << "\":" << std::endl;
+    std::string keywordUpper = keyword;
+    std::transform(keywordUpper.begin(), keywordUpper.end(), keywordUpper.begin(), ::toupper);
+    while(current!= nullptr){
+       
+        std::string descUpper = current->description;
+        std::transform(descUpper.begin(), descUpper.end(), descUpper.begin(), ::toupper);
+        if(descUpper.find(keywordUpper) != std::string::npos){
+            founded = true;
+            if(current->status){
+            std::cout << "ESTADO: COMPLETADA!!!" <<std::endl;    
+        }else{
+            std::cout << "ESTADO: PENDIENTE****" <<std::endl;  
+        }
+        std::cout << "ID DE TAREA: " << current->id <<std::endl;
+        std::cout << "DESCRIPCION: " << current->description << std::endl;
+        std::cout << "AGREGADA EL: "  << current ->dateCreated <<std::endl;
+        std::cout << "------------------------------------" << std::endl;
+        
+        }
+        current = current->next;
+    } 
+    if(!founded){
+        std::cout << "NO HAY TAREAS QUE CONTENGAN LA PALABRA CLAVE: \"" <<keyword <<"\"" << std::endl;
+    }
 
-    return oss.str();
+}
+
+void completeTask(List *&list, int id){
+    
+    Node* current = list->head;
+    Node* previous = nullptr;
+  
+    while (current != nullptr){
+        if(current->id == id){
+            current->status = true;  
+            std::cout << "TAREA MARCADA COMO COMPLETADA" << std::endl;
+            std::cout << "PRESIONE UNA TECLA PARA CONTINUAR...." << std::endl;
+            std::cin.get();
+            return;
+        }
+        previous = current;
+        current = current->next;
+        
+    }
+    std::cout << "LA TAREA CON ESE ID NO EXISTE" << std::endl;
+
+
 }
 
 void showTasks(List *&list){
     if(list->head == nullptr){
-        cout << " LA LISTA ESTA VACIA.. presione cualquier tecla para continuar " << endl;
-        cin.ignore(); cin.get();
+        std::cout << " LA LISTA ESTA VACIA " << std::endl;
+        std::cout << "PRESIONE CUALQUIER TECLA PARA CONTINUAR..." << std::endl;
+        std::cin.ignore(); std::cin.get();
         return;
     }
     Node *aux = list->head;
     while(aux != nullptr){
         if(aux->status){
-            cout << "TAREAS COMPLETADAS: " <<endl;    
+            std::cout << "ESTADO: COMPLETADA!!!" <<std::endl;    
         }else{
-            cout << "TAREAS PENDIENTES: " <<endl;  
+            std::cout << "ESTADO: PENDIENTE****" <<std::endl;  
         }
-        cout << "ID DE TAREA: " << aux->id <<endl;
-        cout << "DESCRIPCION: " << aux->description << endl;
-        cout << "AGREGADA EL: "  << aux ->dateCreated <<endl;
-        cout << "------------------------------------" << endl;
+        std::cout << "ID DE TAREA: " << aux->id <<std::endl;
+        std::cout << "DESCRIPCION: " << aux->description << std::endl;
+        std::cout << "AGREGADA EL: "  << aux ->dateCreated <<std::endl;
+        std::cout << "------------------------------------" << std::endl;
         aux = aux->next;
     }
 }
 
+void emptyList(List *&list){
+    Node * current = list->head;
+    Node * temp;
+    while(current != nullptr){
+        temp = current;
+        current = current->next;
+        delete temp;
+         
+
+    }
+    list->head = nullptr;
+    std::cout << "La lista ha sido vaciada correctamente." << std::endl;
+    std::cout << "PRESIONE UNA TECLA PARA CONTINUAR...."<< std::endl;
+    std::cin.get();
+
+}
+
+
+
+std::string getTimeCustom() {
+    time_t now = time(nullptr);
+    tm* localTime = localtime(&now);
+    std::ostringstream oss;
+    oss << std::setfill('0')
+        << std::setw(2) << localTime->tm_mday << "/"
+        << std::setw(2) << (localTime->tm_mon + 1) << "/"
+        << (localTime->tm_year + 1900)
+        << " -> "
+        << std::setw(2) << localTime->tm_hour << ":"
+        << std::setw(2) << localTime->tm_min;
+
+    return oss.str();
+}
+
+
+
 void runToDo(List *&list){
     int option = -1;
-    string description;
+    std::string description;
     int id;
     char confirmation;
     while (option < 0 || option > 8){
         mainMenu();
-        cin >> option;
+        std::cin >> option;
         switch (option)
         {
         case 1:
             
             option = -1;
-            cout << "Ingrese la descripcion de la tarea: " << endl;
-            cin.ignore();
-            getline(cin,description);
+            std::cout << "Ingrese la descripcion de la tarea: " << std::endl;
+            std::cin.ignore();
+            getline(std::cin,description);
             addTask(list,description);
             option = -1;
             break;
         case 2:
-            cout << "Ingrese el id de la tarea a eliminar (puede ver todas las tareas en la opcion 5)" <<endl;
-            cin >> id;
+            std::cout << "Ingrese el id de la tarea a eliminar (puede ver todas las tareas en la opcion 5)" <<std::endl;
+            std::cin >> id;
             deleteTask(list,id);
             option = -1;
             break;
         case 3:
-            cout << "STILL BEING DEVELOPED" <<endl;
+            std::cout << "Ingrese la descripcion de la tarea o una palabra clave para recuperar varias:" <<std::endl;
+            std::cin.ignore();
+            std::getline(std::cin,description);
+            lookForTask(list,description);
             option = -1;
             break;
         case 4:
-            cout << "STILL BEING DEVELOPED" <<endl;
+            std::cout << "Ingrese el ID de la tarea que ha finalizado: (puede ver todas las tareas en la opcion 5) " << std::endl;
+            std::cin >> id;
+            completeTask(list,id);
             option = -1;
             break;
         case 5:
@@ -207,19 +271,18 @@ void runToDo(List *&list){
             option = -1;
             break;
         case 6:
-            cout << "Se eliminaran todas las tareas de la lista , seguro quiere continuar? [Y/n]" << endl;
-            cin >> confirmation;
-            if(confirmation != 'n' || confirmation != 'N'){
+            std::cout << "Se eliminaran todas las tareas de la lista , seguro quiere continuar? [Y/n]" << std::endl;
+            std::cin >> confirmation;
+            if(confirmation == 'y' || confirmation == 'Y'){
                 emptyList(list);
             }else{
-                cout << "Operacion cancelada... Presione una tecla para continuar" << endl;
-                cin.get();
+                std::cout << "Operacion cancelada... Presione una tecla para continuar" << std::endl;
+                std::cin.get();
             }
-            cout << "STILL BEING DEVELOPED" <<endl;
             option = -1;
             break;
         case 7:
-            cout << "STILL BEING DEVELOPED" <<endl;
+            std::cout << "STILL BEING DEVELOPED" <<std::endl;
             option = -1;
             break;
         default:
--- toDo/toDo.h
+++ toDo/toDo.h
@@ -4,15 +4,14 @@
 
 struct List;
 struct Node;
-
 void mainMenu();
-
 void addTask(List *& list ,std::string description);
-
 std::string getTimeCustom();
-
 void showTasks(List *&list);
-
 void runToDo(List *&list);
+void deleteTask(List *& list, int ID);
+void completeTask(List *&list, int id);
+void emptyList(List *&list);
+void lookForTask(List *list, std::string &keyword);
 
 #endif
